32a6c7af5b8256f2e75914d22d55537b3fb599a6
sebiba <sebiba@gmail.com>
2018-03-27 14:24:23 +0200

    Add files via upload
    envois de la distance un chiffre par un chiffre

--- c/main.c
+++ c/main.c
@@ -1,19 +1,26 @@
 #include <main.h>
 
-int lecture(void);
+int32 lecture(void);
+int32 limite = 0;
 
-void main()
-{
+#INT_RDA
+void RDA_isr(void){
+   //printf("p\r\n");
+   limite = lecture();
+   //printf("changement de limite");
+}
+
+void main(){
+   #define toint(c)  ((c & 0x5F) > '9' ? c - '7' : c - '0')//conversion d'un char en int (0,9)
    setup_timer_0(RTCC_INTERNAL|RTCC_DIV_8|RTCC_8_BIT); // 409us overflow
    setup_timer_1(T1_INTERNAL|T1_DIV_BY_1);      //13,1 ms overflow
    enable_interrupts(GLOBAL);
    setup_low_volt_detect(FALSE);
+   enable_interrupts(INT_RDA);
+   enable_interrupts(GLOBAL);
    float time=0;
-   float distance=0;
-   int limite=0;
-   
-   #define toint(c)  ((c & 0x5F) > '9' ? c - '7' : c - '0')//conversion d'un char en int (0,9)
-   limite = lecture();//demande de la limite de distance 
+   int32 distance=0;
+   printf("sjuygfb");
    while(true){
       output_high(pin_b7); //debut impulsion sur le trigger de la sonde ultrasons
       delay_us(10);
@@ -23,26 +30,37 @@ void main()
       while(input(PIN_b3));//tant que la pin b3 est a high
       time=get_timer1();//recupération du timer
       distance = time*17/100;//calcul de la distance
-      // printf("distance: %f cm\t temps: %f\r\n",distance, time);
-      printf("%f\r\n",distance);//envois de la distance par le port com
-      if(distance>limite){//si la distance est assez grande
-         printf("distance ok\r\n");
+      if(distance<limite){//si la distance est assez grande
+         //printf("O");
+         output_low(PIN_B0);
+         output_high(PIN_B1);
       }else{
-         printf("distance trop courte\r\n");
+         //printf("N");
+         output_low(PIN_B1);
+         output_high(PIN_B0);
       }
+      // printf(":%ld\r\n",distance);
+      char temp[10];
+      int x;
+      sprintf(temp, "%lu", distance);  //conversion du int32 en char
+      //printf("test : %s\r\n", temp);
+      for(x=0;x<9;x++){  //boucle parcourant le char formé du int
+         if(temp[x] != '\0'){  //si la fin du nombre on break
+            printf("%c\r\n"temp[x]);  //envois un chiffre a la fois ;-)
+         }else{
+            break;
+         }
+      }
+      printf(":\r\n");  //signal de fin du chiffre
       delay_ms(300);
    }
-
-
 }
 
-int lecture(){
+int32 lecture(){
       char entre[5];//buffer de 5 characteres
-      int sorti=0,i=0,x=0,y=1;//sorti=int voulu
+      int32 sorti=0;
+      int i=0,x=0,y=1;//sorti=int voulu
       gets(entre);//attent une chaine de charactere fini par CR
-      if(toint(entre[0])>1 || toint(entre[0])==1 && toint(entre[1])>1 && toint(entre[2])>8){
-      printf("Erreur\r\n");//si le int est plus grand que 128 (overflow)
-      }else{
          for(x=0;x<5;x++){//boucle lisant le buffer
             if(entre[x]=='\r'){break;}//quand le char == \r => fin du int
          }
@@ -53,7 +71,5 @@ int lecture(){
          sorti+=toint(entre[0])*y;//la boucle ne veut pas se finir a 0 alors voila :'(
          //printf("int : %d\r\n",sorti);
          return sorti;
-      }
 }
-
 
